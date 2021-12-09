455c1a39e02dedadc2228004373d96e6c6d26366
emorilebo <emorylebo@gmail.com>
2021-12-09 07:35:01 +0100

    ADD my_drawer

---
+++ lib/widgets/my_drawer.dart
@@ -0,0 +1,37 @@
+import 'package:curious_user_app/global/global.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 25, bottom: 10),
+            child: Column(
+              children: [
+                //Header drawer
+                Material(
+                  borderRadius: const BorderRadius.all(Radius.circular(80)),
+                  elevation: 10,
+                  child: Padding(
+                    padding: const EdgeInsets.all(1.0),
+                    child: Container(
+                      height: 160,
+                      width: 160,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            sharedPreferences!.getString('photoUrl')!),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
