b8f96060d0a40f02183923b8221a2c96d8193f46
emorilebo <emorylebo@gmail.com>
2021-12-10 07:57:34 +0100

    UPDATE carousel to home screen

--- lib/mainScreens/home_screen.dart
+++ lib/mainScreens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:curious_user_app/authentication/auth_screen.dart';
 import 'package:curious_user_app/global/global.dart';
 import 'package:curious_user_app/widgets/my_drawer.dart';
@@ -66,7 +67,7 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       drawer: MyDrawer(),
-      body:  CustomScrollView(
+      body: CustomScrollView(
         slivers: [
           SliverToBoxAdapter(
             child: Padding(
@@ -74,7 +75,37 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Container(
                 height: MediaQuery.of(context).size.height * .3,
                 width: MediaQuery.of(context).size.width,
-                child: ,
+                child: CarouselSlider(
+                  options: CarouselOptions(
+                    height: MediaQuery.of(context).size.height * .3,
+                    aspectRatio: 16 / 9,
+                    viewportFraction: 0.8,
+                    initialPage: 0,
+                    enableInfiniteScroll: true,
+                    reverse: false,
+                    autoPlay: true,
+                    autoPlayInterval: const Duration(seconds: 2),
+                    autoPlayAnimationDuration:
+                        const Duration(milliseconds: 500),
+                    autoPlayCurve: Curves.decelerate,
+                    enlargeCenterPage: true,
+                    scrollDirection: Axis.horizontal,
+                  ),
+                  items: items.map((index) {
+                    return Builder(builder: (BuildContext context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: const EdgeInsets.symmetric(horizontal: 1.0),
+                        child: Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Image.asset(
+                            index,
+                          ),
+                        ),
+                      );
+                    });
+                  }).toList(),
+                ),
               ),
             ),
           ),
